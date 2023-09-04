7c4e9ca8ead1ccc3a140ceca75c0550431b41442
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-09-04 11:38:19 +0300

    Completed assignment

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Header({onDarkModeClick})
+{
+    return(
+<header>
+<button onClick={onDarkModeClick}>Dark Mode</button>
+</header>
+    )
+
+    
+}
+
+export default Header;
